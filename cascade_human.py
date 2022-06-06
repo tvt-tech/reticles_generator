d3b550045c630a7c79ff178c6492dbbdef5133a6
Murphy <thehelixpg@gmail.com>
2022-06-06 15:05:09 +0300

    update

--- cascade_human.py
+++ cascade_human.py
@@ -7,45 +7,81 @@ face = cv2.CascadeClassifier("venv/Lib/site-packages/cv2/data/haarcascade_fronta
 
 # cap = cv2.VideoCapture(1)
 #
-img = cv2.imread('pic.jpg')
 
-# while True:
-# success, img = cap.read()
+def draw(cv2, src, dst):
+    fbs = fb.detectMultiScale(src, 1.1, 4)
+    ubs = ub.detectMultiScale(src, 1.1, 4)
+    # lbs = lb.detectMultiScale(img_gray, 1.1, 4)
+    # faces = face.detectMultiScale(inverted_image, 1.1, 4)
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-inverted_image = cv2.bitwise_not(img_gray)
+    for (x, y, w, h) in fbs:
+        cv2.rectangle(dst, (x, y), (x + w, y + h), (0, 0, 255), 1)
+        cv2.line(dst, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (0, 0, 255), 1)
+        cv2.line(dst, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (0, 0, 255), 1)
 
-fbs = fb.detectMultiScale(inverted_image, 1.1, 4)
-ubs = ub.detectMultiScale(inverted_image, 1.1, 4)
-lbs = lb.detectMultiScale(inverted_image, 1.1, 4)
-# faces = face.detectMultiScale(inverted_image, 1.1, 4)
+        distance = int(1000 * 2 / (h / 7))
+        cv2.putText(dst, f'{distance}m', (x, y + h + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, thickness=1, color=(0, 0, 255))
 
-for (x, y, w, h) in fbs:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
-    cv2.line(img, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (0, 0, 255), 1)
-    cv2.line(img, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (0, 0, 255), 1)
+    for (x, y, w, h) in ubs:
+        cv2.rectangle(dst, (x, y), (x + w, y + h), (255, 0, 255), 1)
+        cv2.line(dst, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (255, 0, 255), 1)
+        cv2.line(dst, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (255, 0, 255), 1)
 
-    distance = round(1000 * 2 / (h / 7), 2)
-    print(distance, h)
-    cv2.putText(img, f'{distance}m', (x, y - 3), cv2.FONT_HERSHEY_SIMPLEX, 0.4, thickness=1, color=(255, 255, 255))
+        distance = int(1000 * 1 / (h / 7))
+        cv2.putText(dst, f'{distance}m', (x, y - 3), cv2.FONT_HERSHEY_SIMPLEX, 0.4, thickness=1, color=(255, 0, 255))
 
-for (x, y, w, h) in ubs:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 1)
-    cv2.line(img, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (255, 0, 255), 1)
-    cv2.line(img, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (255, 0, 255), 1)
-for (x, y, w, h) in lbs:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
-    cv2.line(img, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (255, 0, 0), 1)
-    cv2.line(img, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (255, 0, 0), 1)
-# for (x, y, w, h) in faces:
-#     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
-#     cv2.line(img, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (0, 255, 0), 1)
-#     cv2.line(img, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (0, 255, 0), 1)
+    # for (x, y, w, h) in lbs:
+    #     cv2.rectangle(dst, (x, y), (x + w, y + h), (255, 0, 0), 1)
+    #     cv2.line(dst, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (255, 0, 0), 1)
+    #     cv2.line(dst, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (255, 0, 0), 1)
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    #     cv2.line(dst, (x - 3 + int(w / 2), y + int(h / 2)), (x + 3 + int(w / 2), y + int(h / 2)), (0, 255, 0), 1)
+    #     cv2.line(dst, (x + int(w / 2), y - 3 + int(h / 2)), (x + int(w / 2), y + 3 + int(h / 2)), (0, 255, 0), 1)
 
-cv2.imshow('rez', img)
+import os
+import numpy as np
+
+files = os.listdir(os.getcwd())
+files = [f for f in files if f.endswith('.jpg') or f.endswith('.png')]
+
+for f in files:
+
+    img = cv2.imread(f)
+
+    # lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    # l_channel, a, b = cv2.split(lab)
+    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    # cl = clahe.apply(l_channel)
+    # limg = cv2.merge((cl, a, b))
+    # enhanced_img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+    # img = np.vstack((img, enhanced_img))
+
+    # while True:
+    # success, img = cap.read()
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    inverted_image = cv2.bitwise_not(img_gray)
+
+    draw(cv2, img_gray, img)
+    draw(cv2, inverted_image, img)
+
+    cv2.imshow('rez', img)
+
+    cv2.waitKey()
+
+    # cv2.imshow('rez', img_gray)
+    #
+    # cv2.waitKey()
+    #
+    # cv2.imshow('rez', inverted_image)
+    #
+    # cv2.waitKey()
+
+# cv2.imshow('rez', inverted_image)
 # if cv2.waitKey(1) & 0xff == ord('q'):
 #     break
 # pass
-cv2.waitKey()
+
 # cap.release()
 # cv2.destroyAllWindows()
